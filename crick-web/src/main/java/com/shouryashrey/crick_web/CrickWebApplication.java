e307ec0448bff7809d2cb6e94d53398144f7e6d2
Shrey Shourya <shouryashrey@gmail.com>
2025-03-17 22:54:02 +0530

    first successfull POST request

--- crick-web/src/main/java/com/shouryashrey/crick_web/CrickWebApplication.java
+++ crick-web/src/main/java/com/shouryashrey/crick_web/CrickWebApplication.java
@@ -2,12 +2,14 @@ package com.shouryashrey.crick_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.shouryashrey.crick_dao.repos")
 @ComponentScan(basePackages = {"com.shouryashrey.crick_model", "com.shouryashrey.crick_dao.repos", "com.shouryashrey.crick_service", "com.shouryashrey.*", "com.shouryashrey.crick_web"})
+@EntityScan(basePackages = "com.shouryashrey.crick_model")
 public class CrickWebApplication {
 
 	public static void main(String[] args) {
